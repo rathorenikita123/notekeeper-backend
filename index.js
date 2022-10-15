d2d2b1dc963fa98d1f42853cd2dc1b7f795c1790
username <nikitarathore271201@gmail.com>
2022-10-15 14:16:37 +0530

    allowed cors in backend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 
 const start = async () => {
   await db.connect();
-  app.use(cors());
+  app.use(cors({ origin: "http://localhost:3000", credentials: true}));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use('/notes', router);
