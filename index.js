22799350c5f55417defbb5b5c6d5dcb16cfba095
username <nikitarathore271201@gmail.com>
2022-10-16 00:33:23 +0530

    modified backend

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ import express from 'express';
 import router from './routes/index.js';
 import db from './db/index.js';
 import cors from 'cors';
+
 const app = express();
 
 const PORT = process.env.PORT || 8000;
 
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
 const start = async () => {
   await db.connect();
-  app.use(cors({ origin: "http://localhost:3000", credentials: true}));
+  app.use(cors(corsOptions));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use('/notes', router);
